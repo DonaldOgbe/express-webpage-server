213f72268817c10b60e3e33449de9e4b730aae4d
Donald Ogbe <ogbeebimobowei@gmail.com>
2024-07-01 06:58:18 +0100

    handled and add query parameters and personalized endpoint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+app.get("/api/hello", (req, res) => {
+  const visitorName = req.query.visitor_name || "Guest";
+  res.send(`Hello ${visitorName}`);
 });
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Listening for ${port}`);
+  console.log(`Listening for ${port}...`);
 });
