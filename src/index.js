2880563979ba2eba31b2024c9832c6779a030c7f
Donald Ogbe <ogbeebimobowei@gmail.com>
2024-07-01 14:27:46 +0100

    Used IP address to fetch location using external API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 const express = require("express");
+const axios = require("axios");
 const app = express();
 
-app.get("/api/hello", (req, res) => {
+// create GET request handler
+app.get("/api/hello", async (req, res) => {
+  // Extract visitor name
   const visitorName = req.query.visitor_name || "Guest";
-  res.send(`Hello ${visitorName}`);
+
+  // Extract user IP address
+  const clientIP = req.ip;
+
+  // Fetch location from IP address
+
+  try {
+    // Fetch location data from the external API
+
+    const response = await axios.get(`https://ipapi.co/${clientIP}/json/`);
+    const location = response.data.city || "Unknown location";
+
+    res.send({
+      client_IP: clientIP,
+      greeting: `Hello, ${visitorName}`,
+      location: `You are in ${location}`,
+    });
+  } catch (error) {
+    res.send({
+      client_IP: clientIP,
+      greeting: `Hello, ${visitorName}`,
+      location: `Failed to fetch location data`,
+    });
+  }
 });
 
 const port = process.env.PORT || 3000;
